dcbaac3345b2e1b2ae392af746114f0fe6d4c81f
YVZhirnova <YVZhirnova@yandex.ru>
2023-09-24 21:30:47 +0300

    Верстка скелета страницы

---
+++ admpanel/index.php
@@ -0,0 +1,6 @@
+<!--
+<header>
+    <img src="../images/logo.png" alt="Логотип">
+    <h1>Интернет-магазин kinder-shop.ru</h1>
+</header>
+-->
---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no,
+          initial-scale=1.0,
+          maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Интернет-магазин kinder-shop.ru</title>
+    <link rel="icon" href="images/logo.png">
+    <link rel="stylesheet" href="styles/main.css">
+
+</head>
+<body>
+<header>
+    <img src="images/logo.png" alt="Логотип">
+    <h1>Интернет-магазин kinder-shop.ru</h1>
+</header>
+
+<nav>
+    <a href="/">Главная</a>
+    <a href="/admpanel">Административная панель</a>
+</nav>
+
+<main>
+
+</main>
+
+<script src="scripts/main.js"></script>
+
+</body>
+</html>
